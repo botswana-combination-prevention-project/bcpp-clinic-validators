e009a41ebe930851a98931551f7ebb8f7a0d9929
ThinkTankQ <quillerthelon@gmail.com>
2017-06-06 18:07:24 +0200

    Delete __init___BACKUP_27903.py

--- bcpp_clinic_validations/validations/__init___BACKUP_27903.py
+++
@@ -1,8 +0,0 @@
-from .questionnaire import Questionnaire
-<<<<<<< HEAD
-from .clinic_eligibility_form_validation import ClinicEligibilityForm
-# from.clinic_consent import ClinicConsent 
-=======
-from .clinic_subject_locator import ClinicSubjectLocator
-from .viral_load_tracking import ViralLoadTracking
->>>>>>> 4769ed46f272e259ee668974c495b2b89727b61a
