62ed6cca202c1aac3a9417c5bf21b84f4dccab02
aseokana <aseokana@bhp.org.bw>
2017-06-07 16:56:43 +0200

    questionnaire tests

--- bcpp_clinic_validations/tests/test_questionnaire.py
+++ bcpp_clinic_validations/tests/test_questionnaire.py
@@ -1,26 +1,20 @@
 from django.core.exceptions import ValidationError
 from django.test import TestCase
 
-from edc_constants.constants import YES
+from edc_constants.constants import YES, NO
 
 from ..validations import Questionnaire
 
 
 class TestQuestionnaire(TestCase):
 
-    def setUp(self):
-        pass
-
-    def test_knows_last_cd4_n_cd4_count1(self):
-        """Test if knows_last_cd4 is Yes cd4_count is required.
-        """
-        cleaned_data = {'knows_last_cd4': YES, 'cd4_count': 390}
+    def test_knows_last_cd4_yes(self):
+        cleaned_data = {'knows_last_cd4': YES}
         form_validator = Questionnaire(cleaned_data=cleaned_data)
-        self.assertTrue(form_validator.clean())
+        self.assertRaises(ValidationError, form_validator.clean)
 
-    def test_knows_last_cd4_n_cd4_count2(self):
-        """Test if knows_last_cd4 is Yes cd4_count is required.
-        """
-        cleaned_data = {'knows_last_cd4': YES, 'cd4_count': None}
+    def test_knows_last_cd4_no(self):
+        cleaned_data = {'knows_last_cd4': NO,
+                        'cd4_count': 500}
         form_validator = Questionnaire(cleaned_data=cleaned_data)
         self.assertRaises(ValidationError, form_validator.clean)
