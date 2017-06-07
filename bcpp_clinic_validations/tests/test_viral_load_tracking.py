66425d957c226553e85e6fd3e3a589443aa80528
aseokana <aseokana@bhp.org.bw>
2017-06-07 17:12:35 +0200

    viral_loading_tracking tests

--- bcpp_clinic_validations/tests/test_viral_load_tracking.py
+++ bcpp_clinic_validations/tests/test_viral_load_tracking.py
@@ -1,7 +1,7 @@
 from django.core.exceptions import ValidationError
 from django.test import TestCase
 
-from edc_constants.constants import YES
+from edc_constants.constants import YES, NO
 
 from ..validations import ViralLoadTracking
 from edc_base.utils import get_utcnow
@@ -9,19 +9,24 @@ from edc_base.utils import get_utcnow
 
 class TestViralLoadTracking(TestCase):
 
-    def setUp(self):
-        pass
-
-    def test_is_drawn(self):
-        """Test if is_drawn is Yes clinician_initials is required.
-        """
-        cleaned_data = {'is_drawn': YES, 'clinician_initials': None}
+    def test_is_drawn_yes(self):
+        cleaned_data = {'is_drawn': YES}
         form_validator = ViralLoadTracking(cleaned_data=cleaned_data)
         self.assertRaises(ValidationError, form_validator.clean)
 
-    def test_is_drawn_datetime(self):
-        """Test if is_drawn is Yes drawn_datetime is required.
-        """
-        cleaned_data = {'is_drawn': YES, 'drawn_datetime': None}
+    def test_is_drawn_no(self):
+        cleaned_data = {'is_drawn': NO,
+                        'clinician_initials': "XX"}
+        form_validator = ViralLoadTracking(cleaned_data=cleaned_data)
+        self.assertRaises(ValidationError, form_validator.clean)
+
+    def test_is_drawn_datetime_yes(self):
+        cleaned_data = {'is_drawn': YES}
+        form_validator = ViralLoadTracking(cleaned_data=cleaned_data)
+        self.assertRaises(ValidationError, form_validator.clean)
+
+    def test_is_drawn_datetime_no(self):
+        cleaned_data = {'is_drawn': NO,
+                        'drawn_datetime': get_utcnow()}
         form_validator = ViralLoadTracking(cleaned_data=cleaned_data)
         self.assertRaises(ValidationError, form_validator.clean)
