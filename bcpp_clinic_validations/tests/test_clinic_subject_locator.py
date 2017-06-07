97500944e9c3ec9eb7cd4b7438d53088ffeecdf4
aseokana <aseokana@bhp.org.bw>
2017-06-07 11:05:22 +0200

    fixing the tests names

--- bcpp_clinic_validations/tests/test_clinic_subject_locator.py
+++ bcpp_clinic_validations/tests/test_clinic_subject_locator.py
@@ -11,63 +11,63 @@ class TestClinicSubjectLocator(TestCase):
     def setUp(self):
         pass
 
-    def test_home_visit_permission2(self):
+    def test_home_visit_permission(self):
         """Test if home_visit_permission is Yes physical_address is required.
         """
         cleaned_data = {'home_visit_permission': YES, 'physical_address': None}
         clinicsubjectlocator = ClinicSubjectLocator(cleaned_data=cleaned_data)
         self.assertRaises(ValidationError, clinicsubjectlocator.clean)
 
-    def test_may_call_work_place2(self):
+    def test_may_call_work_place(self):
         """Test if may_call_work is Yes subject_work_place is required.
         """
         cleaned_data = {'may_call_work': YES, 'subject_work_place': None}
         clinicsubjectlocator = ClinicSubjectLocator(cleaned_data=cleaned_data)
         self.assertRaises(ValidationError, clinicsubjectlocator.clean)
 
-    def test_may_call_work_phone2(self):
+    def test_may_call_work_phone(self):
         """Test if may_call_work is Yes subject_work_phone is required.
         """
         cleaned_data = {'may_call_work': YES, 'subject_work_phone': None}
         clinicsubjectlocator = ClinicSubjectLocator(cleaned_data=cleaned_data)
         self.assertRaises(ValidationError, clinicsubjectlocator.clean)
 
-    def test_may_follow_up_cell2(self):
+    def test_may_follow_up_cell(self):
         """Test if subject_cell is Yes subject_cell is required.
         """
         cleaned_data = {'may_follow_up': YES, 'subject_cell': None}
         clinicsubjectlocator = ClinicSubjectLocator(cleaned_data=cleaned_data)
         self.assertRaises(ValidationError, clinicsubjectlocator.clean)
 
-    def test_has_alt_contact2(self):
+    def test_has_alt_contact(self):
         """Test if has_alt_contact is Yes alt_contact_name is required.
         """
         cleaned_data = {'has_alt_contact': YES, 'alt_contact_name': None}
         clinicsubjectlocator = ClinicSubjectLocator(cleaned_data=cleaned_data)
         self.assertRaises(ValidationError, clinicsubjectlocator.clean)
 
-    def test_has_alt_contact_rel2(self):
+    def test_has_alt_contact_rel(self):
         """Test if has_alt_contact is Yes alt_contact_rel is required.
         """
         cleaned_data = {'has_alt_contact': YES, 'alt_contact_rel': None}
         clinicsubjectlocator = ClinicSubjectLocator(cleaned_data=cleaned_data)
         self.assertRaises(ValidationError, clinicsubjectlocator.clean)
 
-    def test_may_contact_someone_name2(self):
+    def test_may_contact_someone_name(self):
         """Test if may_contact_someone is Yes contact_name is required.
         """
         cleaned_data = {'may_contact_someone': YES, 'contact_name': None}
         clinicsubjectlocator = ClinicSubjectLocator(cleaned_data=cleaned_data)
         self.assertRaises(ValidationError, clinicsubjectlocator.clean)
 
-    def test_may_contact_someone_rel2(self):
+    def test_may_contact_someone_rel(self):
         """Test if may_contact_someone is Yes contact_rel is required.
         """
         cleaned_data = {'may_contact_someone': YES, 'contact_rel': None}
         clinicsubjectlocator = ClinicSubjectLocator(cleaned_data=cleaned_data)
         self.assertRaises(ValidationError, clinicsubjectlocator.clean)
 
-    def test_may_contact_someone_address2(self):
+    def test_may_contact_someone_address(self):
         """Test if may_contact_someone is Yes contact_physical_address is
         required.
         """
@@ -76,7 +76,7 @@ class TestClinicSubjectLocator(TestCase):
         clinicsubjectlocator = ClinicSubjectLocator(cleaned_data=cleaned_data)
         self.assertRaises(ValidationError, clinicsubjectlocator.clean)
 
-    def test_may_follow_up2(self):
+    def test_may_follow_up(self):
         """If participant has not given permission for follow-up, do not give
         follow-up details
         """
@@ -85,7 +85,7 @@ class TestClinicSubjectLocator(TestCase):
         clinicsubjectlocator = ClinicSubjectLocator(cleaned_data=cleaned_data)
         self.assertRaises(ValidationError, clinicsubjectlocator.clean)
 
-    def test_next_of_kin2(self):
+    def test_next_of_kin(self):
         """If participant has not given permission to contact next_of_kin,
         do not give next_of_kin details
         """
--- bcpp_clinic_validations/tests/test_viral_load_tracking.py
+++ bcpp_clinic_validations/tests/test_viral_load_tracking.py
@@ -4,6 +4,7 @@ from django.test import TestCase
 from edc_constants.constants import YES
 
 from ..validations import ViralLoadTracking
+from edc_base.utils import get_utcnow
 
 
 class TestViralLoadTracking(TestCase):
@@ -11,14 +12,14 @@ class TestViralLoadTracking(TestCase):
     def setUp(self):
         pass
 
-    def test_is_drawn2(self):
+    def test_is_drawn(self):
         """Test if is_drawn is Yes clinician_initials is required.
         """
         cleaned_data = {'is_drawn': YES, 'clinician_initials': None}
         viralloadtracking = ViralLoadTracking(cleaned_data=cleaned_data)
         self.assertRaises(ValidationError, viralloadtracking.clean)
 
-    def test_is_drawn_datetime2(self):
+    def test_is_drawn_datetime(self):
         """Test if is_drawn is Yes drawn_datetime is required.
         """
         cleaned_data = {'is_drawn': YES, 'drawn_datetime': None}
