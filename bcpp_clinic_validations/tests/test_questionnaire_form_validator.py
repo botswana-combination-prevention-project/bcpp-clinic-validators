f0468e4efcf96a40bc0637ed95c67547bc5485ed
erikvw <ew2789@gmail.com>
2017-06-08 12:26:03 +0200

    cleanup questionnare tests

--- bcpp_clinic_validations/tests/test_questionnaire_form_validator.py
+++ bcpp_clinic_validations/tests/test_questionnaire_form_validator.py
@@ -8,20 +8,20 @@ from ..form_validators import QuestionnaireFormValidator
 
 class TestQuestionnaireFormValidator(TestCase):
 
-    def test_knows_last_cd4_yes(self):
-        cleaned_data = {'knows_last_cd4': YES}
+    def test_knows_last_cd4_yes1(self):
+        cleaned_data = {'knows_last_cd4': YES, 'cd4_count': None}
         form_validator = QuestionnaireFormValidator(cleaned_data=cleaned_data)
         self.assertRaises(ValidationError, form_validator.clean)
+        self.assertIsNotNone(form_validator._errors.get('cd4_count'))
+
+    def test_knows_last_cd4_yes2(self):
+        cleaned_data = {'knows_last_cd4': YES, 'cd4_count': 500}
+        form_validator = QuestionnaireFormValidator(cleaned_data=cleaned_data)
+        self.assertIsNone(form_validator._errors.get('cd4_count'))
 
     def test_knows_last_cd4_no(self):
         cleaned_data = {'knows_last_cd4': NO,
                         'cd4_count': 500}
         form_validator = QuestionnaireFormValidator(cleaned_data=cleaned_data)
         self.assertRaises(ValidationError, form_validator.clean)
-
-    def test_knows_last_cd4_true(self):
-        cleaned_data = {
-            'knows_last_cd4': YES,
-            'cd4_count': None}
-        form_validator = QuestionnaireFormValidator(cleaned_data=cleaned_data)
-        self.assertRaises(ValidationError, form_validator.clean)
+        self.assertIsNotNone(form_validator._errors.get('cd4_count'))
