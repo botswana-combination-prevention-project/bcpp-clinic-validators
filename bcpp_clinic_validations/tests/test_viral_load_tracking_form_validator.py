110a42d37df3596289f507d84f7f169936a07663
erikvw <ew2789@gmail.com>
2017-06-08 12:19:30 +0200

    update VL tests

--- bcpp_clinic_validations/tests/test_viral_load_tracking_form_validator.py
+++ bcpp_clinic_validations/tests/test_viral_load_tracking_form_validator.py
@@ -5,11 +5,12 @@ from edc_base.utils import get_utcnow
 from edc_constants.constants import YES, NO
 
 from ..form_validators import ViralLoadTrackingFormValidator
+from pprint import pprint
 
 
 class TestViralLoadTrackingFormValidator(TestCase):
 
-    def test_if_drawn_yes_datetime_required(self):
+    def test_if_drawn_datetime_required(self):
         cleaned_data = {
             'is_drawn': YES,
             'drawn_datetime': None,
@@ -19,7 +20,7 @@ class TestViralLoadTrackingFormValidator(TestCase):
         self.assertRaises(ValidationError, form_validator.clean)
         self.assertIsNotNone(form_validator._errors.get('drawn_datetime'))
 
-    def test_if_not_drawn_datetime_not_required(self):
+    def test_if_not_drawn_datetime_not_required1(self):
         cleaned_data = {
             'is_drawn': NO,
             'drawn_datetime': None,
@@ -30,8 +31,22 @@ class TestViralLoadTrackingFormValidator(TestCase):
         self.assertIsNone(form_validator._errors.get('clinician_initials'))
 
     def test_if_not_drawn_datetime_not_required2(self):
+        """Asserts raises and raises on clinician_initials, first.
+        """
         cleaned_data = {'is_drawn': NO,
-                        'drawn_datetime': get_utcnow()}
+                        'drawn_datetime': get_utcnow(),
+                        'clinician_initials': 'XX'}
+        form_validator = ViralLoadTrackingFormValidator(
+            cleaned_data=cleaned_data)
+        self.assertRaises(ValidationError, form_validator.clean)
+        self.assertIsNotNone(form_validator._errors.get('clinician_initials'))
+
+    def test_if_not_drawn_datetime_not_required3(self):
+        """Asserts raises and raises on drawn_datetime, second.
+        """
+        cleaned_data = {'is_drawn': NO,
+                        'drawn_datetime': get_utcnow(),
+                        'clinician_initials': None}
         form_validator = ViralLoadTrackingFormValidator(
             cleaned_data=cleaned_data)
         self.assertRaises(ValidationError, form_validator.clean)
