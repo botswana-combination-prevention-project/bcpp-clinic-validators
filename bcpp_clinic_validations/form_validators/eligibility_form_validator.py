56aa84319b338f717a29205104bc0370f86cd2bd
Coulson Thabo Kgathi <ckgathi@gmail.com>
2017-06-14 15:49:18 +0200

    remove irrelevant tests and validator

--- bcpp_clinic_validations/form_validators/eligibility_form_validator.py
+++ bcpp_clinic_validations/form_validators/eligibility_form_validator.py
@@ -1,45 +1,6 @@
 from edc_base.modelform_validators import FormValidator
-from edc_constants.constants import YES
 
 
 class EligibilityFormValidator(FormValidator):
-
-    def clean(self):
-
-        self.required_if(
-            YES,
-            field='has_identity',
-            field_required='identity',
-        )
-
-        self.required_if(
-            YES,
-            field='clinic_household_member',
-            field_required='first_name',
-        )
-
-        self.required_if(
-            YES,
-            field='clinic_household_member',
-            field_required='initials',
-        )
-
-        self.required_if(
-            YES,
-            field='clinic_household_member',
-            field_required='report_datetime',
-        )
-
-        self.required_if(
-            YES,
-            field='clinic_household_member',
-            field_required='age_in_years',
-        )
-
-        self.required_if(
-            YES,
-            field='clinic_household_member',
-            field_required='gender',
-        )
-
-        return self.cleaned_data
+    # TODO: Add the right validations
+    pass
--- bcpp_clinic_validations/tests/test_eligibility_form_validator.py
+++ bcpp_clinic_validations/tests/test_eligibility_form_validator.py
@@ -1,47 +1,5 @@
-from django.core.exceptions import ValidationError
 from django.test import TestCase
-
-from edc_constants.constants import YES, NO
-
-from ..form_validators import EligibilityFormValidator
 
 
 class TestClinicEligibilityFormValidator(TestCase):
-
-    def test_has_identity_yes1(self):
-        cleaned_data = {'has_identity': YES, 'identity': None}
-        form_validator = EligibilityFormValidator(cleaned_data=cleaned_data)
-        self.assertRaises(ValidationError, form_validator.clean)
-        self.assertIsNotNone(form_validator._errors.get('identity'))
-
-    def test_has_identity_yes2(self):
-        cleaned_data = {'has_identity': YES, 'identity': 21222221222}
-        form_validator = EligibilityFormValidator(cleaned_data=cleaned_data)
-        self.assertRaises(ValidationError, form_validator.clean)
-        self.assertIsNone(form_validator._errors.get('identity'))
-
-    def test_has_identity_no(self):
-        cleaned_data = {'has_identity': NO, 'has_identity': 21222221222}
-        form_validator = EligibilityFormValidator(cleaned_data=cleaned_data)
-        self.assertRaises(ValidationError, form_validator.clean)
-        self.assertIsNotNone(form_validator._errors.get('identity'))
-
-    def test_first_name_yes1(self):
-        cleaned_data = {'clinic_household_member': NO, 'first_name': None}
-        form_validator = EligibilityFormValidator(cleaned_data=cleaned_data)
-        self.assertRaises(ValidationError, form_validator.clean)
-        self.assertIsNotNone(form_validator._errors.get('first_name'))
-
-    def test_first_name_yes2(self):
-        cleaned_data = {'clinic_household_member': YES,
-                        'first_name': 'Allan'}
-        form_validator = EligibilityFormValidator(cleaned_data=cleaned_data)
-        self.assertRaises(ValidationError, form_validator.clean)
-        self.assertIsNone(form_validator._errors.get('first_name'))
-
-    def test_first_name_no(self):
-        cleaned_data = {'clinic_household_member': NO,
-                        'first_name': 'Allan'}
-        form_validator = EligibilityFormValidator(cleaned_data=cleaned_data)
-        self.assertRaises(ValidationError, form_validator.clean)
-        self.assertIsNotNone(form_validator._errors.get('first_name'))
+    pass
