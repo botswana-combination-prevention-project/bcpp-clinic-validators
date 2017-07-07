59c96bf0c400bdf8910e161f45f75b8c146e554f
Gofaone Magodi <magodign@magodign.local>
2017-07-07 10:27:08 +0200

    redmine_2900,2902,2898, issues fixes

--- bcpp_clinic_validations/form_validators/questionnaire_form_validator.py
+++ bcpp_clinic_validations/form_validators/questionnaire_form_validator.py
@@ -1,38 +1,60 @@
+from django import forms
+from edc_constants.constants import YES, DWTA, NEG, NO, POS
+
 from edc_base.modelform_validators import FormValidator
-from edc_constants.constants import YES, DWTA, NEG, UNKNOWN
 
 
 class QuestionnaireFormValidator(FormValidator):
 
     def clean(self):
+        cleaned_data = super().clean()
+        self.validate_hiv_status()
+        self.currently_taking_ARVS()
+        return cleaned_data
 
-        self.required_if(
-            YES,
-            field='know_hiv_status',
-            field_required='current_hiv_status')
+    def validate_hiv_status(self):
+        cleaned_data = self.cleaned_data
+        if cleaned_data.get('know_hiv_status') == NO:
+            if cleaned_data.get('current_hiv_status') in [POS, NEG]:
+                raise forms.ValidationError(
+                    'PPT does not know HIV Status Expecting Unknown')
 
-        self.not_required_if(
-            DWTA,
-            field='know_hiv_status',
-            field_required='current_hiv_status')
+            if cleaned_data.get('on_arv') in [DWTA, NO, YES]:
+                raise forms.ValidationError(
+                    'PPT cannot take ARV while HIV status is Unknown')
 
-        self.not_applicable_if(
-            UNKNOWN,
-            field='know_hiv_status',
-            field_required='current_hiv_status')
+        elif cleaned_data.get('know_hiv_status') == YES:
+            if cleaned_data.get('current_hiv_status') not in [POS, NEG]:
+                raise forms.ValidationError(
+                    'PPT has to specify the HIV result since known status is YES')
 
-        self.required_if(
-            YES,
-            field='on_arv',
-            field_required='arv_evidence')
+            if (cleaned_data.get('current_hiv_status') == NEG and
+                    cleaned_data.get('on_arv') == YES):
+                raise forms.ValidationError(
+                    'PPT cannot be on ART while HIV NEGATIVE')
+
+        return cleaned_data
+
+    def currently_taking_ARVS(self):
+        cleaned_data = self.cleaned_data
+        if cleaned_data.get('on_arv') == NO:
+            if cleaned_data.get('arv_evidence') in [DWTA, NO, YES]:
+                raise forms.ValidationError(
+                    'PPT cannot have ARV evidence when not on ARV')
+
+        elif cleaned_data.get('on_arv') == YES:
+            if cleaned_data.get('arv_evidence') not in [NO, YES]:
+                raise forms.ValidationError(
+                    'PPT has to specify ARV Evidence since PPT is on ARV')
 
         self.required_if(
             YES,
             field='knows_last_cd4',
             field_required='cd4_count')
 
-        self.not_applicable_if(
-            NEG,
-            field='current_hiv_status',
-            field_applicable='arv_evidence')
-#        return self.cleaned_data
+        self.not_required_if(
+            NO,
+            field='knows_last_cd4',
+            field_required='cd4_count')
+
+        return cleaned_data
--- bcpp_clinic_validations/tests/test_questionnaire_form_validator.py
+++ bcpp_clinic_validations/tests/test_questionnaire_form_validator.py
@@ -1,30 +1,23 @@
 from django.core.exceptions import ValidationError
 from django.test import TestCase, tag
-
 from edc_constants.constants import YES, NO, DWTA, NEG
 
-from ..form_validators import QuestionnaireFormValidator
 from bcpp_clinic_validations import form_validators
+
+from ..form_validators import QuestionnaireFormValidator
 
 
 class TestQuestionnaireFormValidator(TestCase):
+
     def test_know_hiv_status_yes(self):
         cleaned_data = {'know_hiv_status': YES,
                         'current_hiv_status': None}
         form_validator = QuestionnaireFormValidator(cleaned_data=cleaned_data)
         self.assertRaises(ValidationError, form_validator.clean)
-        self.assertIsNotNone(form_validator._errors.get('current_hiv_status'))
-
-    def test_know_hiv_status_dwta(self):
-        cleaned_data = {'know_hiv_status': DWTA,
-                        'current_hiv_status': 'NEGATIVE'}
-        form_validator = QuestionnaireFormValidator(cleaned_data=cleaned_data)
-        self.assertIsNone(form_validator._errors.get('current_hiv_status'))
-        self.assertRaises(ValidationError, form_validator.clean)
 
     def test_know_hiv_status_no(self):
         cleaned_data = {'know_hiv_status': NO,
-                        'current_hiv_status': 'NEGATIVE'}
+                        'current_hiv_status': NEG}
         form_validator = QuestionnaireFormValidator(cleaned_data=cleaned_data)
         self.assertIsNone(form_validator._errors.get('current_hiv_status'))
         self.assertRaises(ValidationError, form_validator.clean)
@@ -34,7 +27,6 @@ class TestQuestionnaireFormValidator(TestCase):
                         'arv_evidence': None}
         form_validator = QuestionnaireFormValidator(cleaned_data=cleaned_data)
         self.assertRaises(ValidationError, form_validator.clean)
-        self.assertIsNotNone(form_validator._errors.get('arv_evidence'))
 
     def test_knows_last_cd4_yes1(self):
         cleaned_data = {'knows_last_cd4': YES, 'cd4_count': None}
@@ -51,11 +43,15 @@ class TestQuestionnaireFormValidator(TestCase):
         cleaned_data = {'knows_last_cd4': NO, 'cd4_count': 500}
         form_validator = QuestionnaireFormValidator(cleaned_data=cleaned_data)
         self.assertRaises(ValidationError, form_validator.clean)
-        self.assertIsNotNone(form_validator._errors.get('cd4_count'))
 
     def test_current_hiv_status_neg(self):
         cleaned_data = {'current_hiv_status': NEG,
-                        'arv_evidence': None}
+                        'on_arv': YES}
         form_validator = QuestionnaireFormValidator(cleaned_data=cleaned_data)
         self.assertRaises(ValidationError, form_validator.clean)
-        self.assertIsNotNone(form_validator._errors.get('arv_evidence'))
+
+    def test_yes_on_arvs_applicable_evidence(self):
+        cleaned_data = {'on_arv': NO,
+                        'arv_evidence': YES}
+        form_validator = QuestionnaireFormValidator(cleaned_data=cleaned_data)
+        self.assertRaises(ValidationError, form_validator.clean)
