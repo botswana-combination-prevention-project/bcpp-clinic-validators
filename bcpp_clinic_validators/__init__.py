b438f79efdd4de19129c4b0f9f754e73b2f06997
erikvw <ew2789@gmail.com>
2017-08-08 22:29:15 +0200

    visit validator

--- bcpp_clinic_validators/__init__.py
+++ bcpp_clinic_validators/__init__.py
@@ -1,4 +1,5 @@
 from .form_validators import QuestionnaireFormValidator
 from .form_validators import SubjectEligibilityFormValidator
 from .form_validators import SubjectLocatorFormValidator
+from .form_validators import SubjectVisitFormValidator
 from .form_validators import ViralLoadTrackingFormValidator
