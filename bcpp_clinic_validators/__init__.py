0408e8c8662e04a714ba74d30ab4bb229e5cd9c9
erikvw <ew2789@gmail.com>
2017-08-08 11:48:53 +0200

    -

--- bcpp_clinic_validators/__init__.py
+++ bcpp_clinic_validators/__init__.py
@@ -1,4 +1,4 @@
-from .form_validators import EligibilityFormValidator
 from .form_validators import QuestionnaireFormValidator
+from .form_validators import SubjectEligibilityFormValidator
 from .form_validators import SubjectLocatorFormValidator
 from .form_validators import ViralLoadTrackingFormValidator
--- bcpp_clinic_validators/form_validators/subject_eligibility.py
+++ bcpp_clinic_validators/form_validators/subject_eligibility.py
@@ -1,7 +1,6 @@
-from django import forms
-
 from edc_base.modelform_validators import FormValidator
 from edc_constants.constants import YES, DWTA, NO, POS
 
 
 class SubjectEligibilityFormValidator(FormValidator):
+    pass
